4b044c2bcc1ad9034ce0a6d0d48b1ade604a77f0
Desert0rluz <luizmartinsluz@gmail.com>
2024-07-24 21:59:27 -0300

    salvamento parcial de progresso

---
+++ Users.Api/Data/UsersDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Users.Api.Data;
+
+public class UsersDbContext : DbContext
+{
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
